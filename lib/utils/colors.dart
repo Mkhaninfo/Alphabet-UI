0179c71e66dcf28fcf0df70beea55a56909da732
Mkhaninfu <you@example.com>
2022-01-04 17:32:45 +0600

    final

---
+++ lib/utils/colors.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: constant_identifier_names
+
+import 'package:flutter/material.dart';
+
+class ColorManager {
+  static const List<Color> PRIMARY_LIST = [
+    Color(0xFFFF1414),
+    Color(0xFF0082CC),
+    Color(0xFFFF4500),
+    Color(0xFF800080),
+    Color(0xFF001B6E),
+  ];
+}
